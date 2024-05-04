91db542d2631f45dd4c59bd8d585d9bc003aad3e
Pavel Kopychenko <t34box@gmail.com>
2024-05-04 14:53:02 +0300

    Paste key for alacritty and cider|clojure settings.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,31 +1,44 @@
-{:user {:plugins      [[lein-ancient "0.7.0"]]
-        :dependencies [[nrepl "0.9.0"]
-                       [refactor-nrepl "3.5.2"]
-                       [cider/cider-nrepl "0.28.3"]
-                       [cider/piggieback "0.4.0"]]
+{:user {:plugins      [[nrepl "1.1.1"]
+                       [lein-ancient "0.7.0"]
+                       [refactor-nrepl "3.10.0"]
+                       [cider/cider-nrepl "0.45.0"]]
+        :dependencies [
+                       ;;[cider/piggieback "0.4.0"]
+                       ]
         :repl-options {:init             (set! *print-length* 100)
-                       :nrepl-middleware [cider.nrepl/wrap-apropos
+                       :nrepl-middleware [
+                                          cider.nrepl/wrap-apropos
                                           cider.nrepl/wrap-classpath
+                                          cider.nrepl/wrap-clojuredocs
                                           cider.nrepl/wrap-complete
+                                          cider.nrepl/wrap-content-type
                                           cider.nrepl/wrap-debug
+                                          cider.nrepl/wrap-enlighten
                                           cider.nrepl/wrap-format
                                           cider.nrepl/wrap-info
                                           cider.nrepl/wrap-inspect
+                                          cider.nrepl/wrap-log
                                           cider.nrepl/wrap-macroexpand
                                           cider.nrepl/wrap-ns
-                                          cider.nrepl/wrap-spec
+                                          cider.nrepl/wrap-out
+                                          cider.nrepl/wrap-slurp
                                           cider.nrepl/wrap-profile
                                           cider.nrepl/wrap-refresh
+                                          cider.nrepl/wrap-reload
                                           cider.nrepl/wrap-resource
+                                          cider.nrepl/wrap-spec
                                           cider.nrepl/wrap-stacktrace
                                           cider.nrepl/wrap-test
                                           cider.nrepl/wrap-trace
-                                          cider.nrepl/wrap-out
+                                          cider.nrepl/wrap-tracker
                                           cider.nrepl/wrap-undef
                                           cider.nrepl/wrap-version
-                                          refactor-nrepl.middleware/wrap-refactor]}
-        :figwheel     {:nrepl-middleware
-                       [refactor-nrepl.middleware/wrap-refactor
-                        cider.nrepl/cider-middleware
-                        ;;cemerick.piggieback/wrap-cljs-repl
-                        ]}}}}
+                                          cider.nrepl/wrap-xref
+
+                                          refactor-nrepl.middleware/wrap-refactor
+                                          ]}
+        :figwheel {:nrepl-middleware
+                   [refactor-nrepl.middleware/wrap-refactor
+                    cider.nrepl/cider-middleware
+                    ;;cemerick.piggieback/wrap-cljs-repl
+                    ]}}}}
