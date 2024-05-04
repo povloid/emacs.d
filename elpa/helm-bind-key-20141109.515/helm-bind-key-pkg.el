539e9f77f0659e3fd85476c7c5e3d83da94cf7e4
pacman <pacman@localhost.localdomain>
2024-05-04 10:19:09 +0300

    New package to elpa

---
+++ elpa/helm-bind-key-20141109.515/helm-bind-key-pkg.el
@@ -0,0 +1,14 @@
+(define-package "helm-bind-key" "20141109.515" "helm-source for for bind-key."
+  '((bind-key "1.0")
+    (helm "1.6.4"))
+  :commit "9da6ad8b7530e72fb4ac67be8c6a482898dddc25" :authors
+  '(("Yuhei Maeda <yuhei.maeda_at_gmail.com>"))
+  :maintainers
+  '(("myuhe"))
+  :maintainer
+  '("myuhe")
+  :keywords
+  '("convenience" "emulation"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
