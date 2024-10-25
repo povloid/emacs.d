d0e88d7d6125b58c1f92df93ae1b7a9f8c39d841
Pavel Kopychenko <pkopychenko@inko-med.ru>
2024-10-25 16:45:22 +0300

    Next config for vscode and gnome

--- linux/gnome/set-keybord-swith-shortkats.sh
+++ linux/gnome/set-keybord-swith-shortkats.sh
@@ -1,1 +1,7 @@
-gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['<Shift>Alt_L','<Shift>Alt_R']"
+#!/bin/bash
+
+
+gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps,grp_led:scroll']"
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['<Control>Shift_R']"
+
+# gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['<Shift>Alt_L','<Shift>Alt_R']"
